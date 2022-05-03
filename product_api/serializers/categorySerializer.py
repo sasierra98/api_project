790ec06cc0eb803e528f267cec1f69e51aabba4d
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2022-05-02 21:03:56 -0500

    [ADD] CategorySerializer and ProductSerializer added

---
+++ product_api/serializers/categorySerializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from product_api.models.category import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
---
+++ product_api/serializers/productSerializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from product_api.models.product import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'category']
