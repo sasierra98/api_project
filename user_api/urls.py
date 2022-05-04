7b75ac47c11596fdd6c0ad72325f9992a3ba04cf
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2022-05-03 22:40:43 -0500

    [ADD] supplier/ and supplier/id added in url

--- user_api/urls.py
+++ user_api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from user_api import views
+from user_api.views.user import UserApiView, UserDetailView
+from user_api.views.supplier import SupplierApiView, SupplierDetailView
 
 urlpatterns = [
-    path('user/', views.UserApiView.as_view()),
-    path('user/<int:pk>/', views.UserDetailView.as_view())
+    path('user/', UserApiView.as_view()),
+    path('user/<int:pk>/', UserDetailView.as_view()),
+    path('supplier/', SupplierApiView.as_view()),
+    path('supplier/<int:pk>', SupplierDetailView.as_view())
 ]
